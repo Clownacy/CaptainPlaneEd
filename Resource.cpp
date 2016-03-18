dcf8305351e15cef4dda64d614e1d792e1da5a2f
Clownacy <Clownacy@users.noreply.github.com>
2016-03-18 22:12:34 +0000

    Making use of the 'this' pointer
    It makes things clearer, I guess.

--- Resource.cpp
+++ Resource.cpp
@@ -15,7 +15,7 @@
 #define FILE_MAP_DEFAULT "MapDefault.bin"
 
 void Resource::Save(const char* const filename, const char* const dstfilename) {
-    CompressFile(compression, filename, dstfilename);
+    CompressFile(this->compression, filename, dstfilename);
 }
 
 long Resource::DecompressFile(const comprType compr_type, const char* const srcfile, const char* const dstfile, const long Pointer, const int length)
@@ -64,10 +64,10 @@ void Resource::CompressFile(const comprType compr_type, const char* const srcfil
 			enigma::encode(srcfile, dstfile, false);
 			break;
 		case comprType::KOSINSKI:
-			kosinski::encode(srcfile, dstfile, false, kosinski_module_size, 16u);
+			kosinski::encode(srcfile, dstfile, false, this->kosinski_module_size, 16u);
 			break;
 		case comprType::MODULED_KOSINSKI:
-			kosinski::encode(srcfile, dstfile, true, kosinski_module_size, 16u);
+			kosinski::encode(srcfile, dstfile, true, this->kosinski_module_size, 16u);
 			break;
 		case comprType::NEMESIS:
 			nemesis::encode(srcfile, dstfile);
@@ -83,38 +83,38 @@ void Resource::CompressFile(const comprType compr_type, const char* const srcfil
 
 void ResourceArt::Load(const char* const filename)
 {
-	if (compression == comprType::INVALID)
+	if (this->compression == comprType::INVALID)
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Invalid art compression format. Should be one of the following:\n\n'None'\n'Enigma'\n'Kosinski'\n'Moduled Kosinski'\n'Nemesis'\n'Kid Chameleon'\n'Comper'\n'Saxman'", NULL);
 		exit(1);
 	}
 
-	length = DecompressFile(compression, name, filename, offset, length);
+	this->length = DecompressFile(this->compression, this->name, filename, this->offset, this->length);
 
-	if (length < 0)
+	if (this->length < 0)
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Could not decompress art file. Are you sure the compression is correct?", NULL);
 		exit(1);
 	}
-	tileAmount = length/0x20;
+	tileAmount = this->length/0x20;
 }
 
 void ResourceMap::Load(const char* const filename)
 {
-	if (compression == comprType::INVALID || compression == comprType::KID_CHAMELEON)
+	if (this->compression == comprType::INVALID || this->compression == comprType::KID_CHAMELEON)
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Invalid map compression format. Should be one of the following:\n\n'None'\n'Enigma'\n'Kosinski'\n'Moduled Kosinski'\n'Nemesis'\n'Comper'\n'Saxman'", NULL);
 		exit(1);
 	}
 
-	length = DecompressFile(compression, name, filename, offset, length);
+	this->length = DecompressFile(this->compression, this->name, filename, this->offset, this->length);
 
 	FILE* mapfile = fopen(filename, "r+b");
 
-	if (length < 0) {
+	if (this->length < 0) {
 		//file could not be decompressed or found
-		length = 2*xSize*ySize;
-		if (!CheckCreateBlankFile(name, mapfile, offset, length))
+		this->length = 2*this->xSize*this->ySize;
+		if (!CheckCreateBlankFile(this->name, mapfile, this->offset, this->length))
 		{
 			//file is existant but could not be decompressed
 			SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Could not decompress map file. Are you sure the compression is correct?", NULL);
@@ -129,34 +129,34 @@ void ResourceMap::Load(const char* const filename)
 
 	fclose(mapfile);
 
-	if (length < 2*xSize*ySize)
+	if (this->length < 2*this->xSize*this->ySize)
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_WARNING, "Warning", "Specified size exceeds map size.\nField has been trimmed vertically.", NULL);
-		ySize = (length/xSize) / 2;
-		if (ySize == 0)
+		this->ySize = (this->length/this->xSize) / 2;
+		if (this->ySize == 0)
 			exit(1);
 	}
     
-	if (strlen(saveName) == 0)
+	if (strlen(this->saveName) == 0)
 	{
-		if (offset == 0)
-			strcpy(saveName, name); //overwrite existing map
+		if (this->offset == 0)
+			strcpy(this->saveName, this->name); //overwrite existing map
 		else
-			strcpy(saveName, FILE_MAP_DEFAULT); //write to default file
+			strcpy(this->saveName, FILE_MAP_DEFAULT); //write to default file
 	}
 }
 
 void ResourcePal::Load(const char* const filename)
 {
-	if (compression == comprType::INVALID)
+	if (this->compression == comprType::INVALID)
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Invalid palette compression format. Should be one of the following:\n\n'None'\n'Enigma'\n'Kosinski'\n'Moduled Kosinski'\n'Nemesis'\n'Kid Chameleon'\n'Comper'\n'Saxman'", NULL);
 		exit(1);
 	}
 
-	length = DecompressFile(compression, name, filename, offset, length);
+	this->length = DecompressFile(this->compression, this->name, filename, this->offset, this->length);
 
-	if (length < 0)
+	if (this->length < 0)
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Could not decompress palette file. Are you sure the compression is correct?", NULL);
 		exit(1);
