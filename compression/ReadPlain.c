a22b617629bc602af136785bb3b2067d3f08befe
Clownacy <Clownacy@users.noreply.github.com>
2016-03-18 21:28:40 +0000

    Trying to patch up ReadPlain's many holes
    Cppcheck went mad when it scanned that thing

--- compression/ReadPlain.c
+++ compression/ReadPlain.c
@@ -9,24 +9,35 @@ long ReadPlain(const char* const srcfile, const char* const dstfile, long Pointe
 {
     FILE* src = fopen(srcfile, "rb");
     FILE* dst = fopen(dstfile, "w+b");
-    if (src == NULL || dst == NULL) return -1;
+    if (src == NULL || dst == NULL)
+    {
+        if (src != NULL)
+            fclose(src);
+        if (dst != NULL)
+            fclose(dst);
+        return -1;
+    }
 
     if (length == 0) {
         if (Pointer == 0) {
             fseek(src, 0, SEEK_END);
             length = ftell(src);
             rewind(src);
-        } else return -1;
+        } else {
+            fclose(src);
+            fclose(dst);
+            return -1;
+        }
     }
-    
+
     fseek(src, Pointer, SEEK_SET);
     rewind(dst);
 
     char* buffer = (char*) malloc(length);
     fread(buffer, length, 1, src);
     fwrite(buffer, length, 1, dst);
-    //free(buffer);
-    
+    free(buffer);
+
     fclose(src);
     fclose(dst);
     return length;
