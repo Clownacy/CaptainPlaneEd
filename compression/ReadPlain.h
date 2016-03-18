2cf484649711ba624f6966c08c5363f1fab134fd
Clownacy <Clownacy@users.noreply.github.com>
2016-03-18 18:27:09 +0000

    Added missing include

--- compression/ReadPlain.h
+++ compression/ReadPlain.h
@@ -4,6 +4,8 @@
 extern "C" {
 #endif
 
+#include <stdio.h>
+
 long ReadPlain(const char* const srcfile, const char* const dstfile, long Pointer, int length);
 bool CheckCreateBlankFile(char *srcfile, FILE* dst, long Pointer, int length);
 
