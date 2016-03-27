41a05b58dd2d6dc68efa4210d305f93a9819a290
Clownacy <Clownacy@users.noreply.github.com>
2016-03-27 21:21:35 +0100

    Applied stylisation to header file

--- Graphics.h
+++ Graphics.h
@@ -30,39 +30,39 @@ public:
     Graphics(uint16_t xSize, uint16_t tileOffset, uint16_t tileAmount);
     void ReadPalette(const char* const filename);
     void ReadTiles(const char* const filename);
-    void CreateTiles();
-    SDL_Surface* InitSurface(uint16_t *pixelsT, int width, int height, int bbp);
-    void DrawSurface(SDL_Surface *img, SDL_Surface *screen, int x, int y);
-    void ClearMap();
-    void ClearSelector();
-    void DrawSelector();
+    void CreateTiles(void);
+    SDL_Surface* InitSurface(uint16_t* pixelsT, int width, int height, int bbp);
+    void DrawSurface(SDL_Surface* img, SDL_Surface* screen, int x, int y);
+    void ClearMap(void);
+    void ClearSelector(void);
+    void DrawSelector(void);
     bool CheckSelValidPos(int x, int y);
     void DrawTileSingle(int x, int y, Tile tile);
     void DrawTileBlank(int x, int y, Tile tile);
     void DrawTileFullColor(int x, int y, uint32_t color);
     void DrawTileNone(int x, int y);
     void DrawTileInvalid(int x, int y);
-    void ProcessDisplay();
+    void ProcessDisplay(void);
     void SetCurrentPal(uint8_t currentPal) {this->currentPal = currentPal;}
-    uint8_t GetCurrentPal() {return currentPal;}
-    uint8_t GetPaletteLines() {return paletteLines;}
-    uint8_t GetSelectorWidth() {return selectorWidth;}
-    uint16_t GetTileAmount() {return tileAmount;}
-    uint16_t GetTileOffset() {return tileOffset;}
-    void ToggleHighPriority() {highPriorityDisplay = !highPriorityDisplay;}
-    void ToggleLowPriority() {lowPriorityDisplay = !lowPriorityDisplay;}
+    uint8_t GetCurrentPal(void) {return currentPal;}
+    uint8_t GetPaletteLines(void) {return paletteLines;}
+    uint8_t GetSelectorWidth(void) {return selectorWidth;}
+    uint16_t GetTileAmount(void) {return tileAmount;}
+    uint16_t GetTileOffset(void) {return tileOffset;}
+    void ToggleHighPriority(void) {highPriorityDisplay = !highPriorityDisplay;}
+    void ToggleLowPriority(void) {lowPriorityDisplay = !lowPriorityDisplay;}
     void DrawPixel(int x, int y);
     void DrawRect(int x, int y); //x, y = tile coordinates (not pixel)
     void DrawFreeRect(int x, int y, int xSize, int ySize);
-    void IncScreenOffset() {++screenTileYOffset;}
-    void DecScreenOffset() {if (screenTileYOffset>0) --screenTileYOffset;}
-    int GetScreenOffset() {return screenTileYOffset;}
-    void IncScreenXOffset() {++screenTileXOffset;}
-    void DecScreenXOffset() {if (screenTileXOffset>0) --screenTileXOffset;}
-    int GetScreenXOffset() {return screenTileXOffset;}
-    void IncSelOffset() {++selTileYOffset;}
-    void DecSelOffset() {if (selTileYOffset>0) --selTileYOffset;}
-    int GetSelOffset() {return selTileYOffset;}
+    void IncScreenOffset(void) {++screenTileYOffset;}
+    void DecScreenOffset(void) {if (screenTileYOffset>0) --screenTileYOffset;}
+    int GetScreenOffset(void) {return screenTileYOffset;}
+    void IncScreenXOffset(void) {++screenTileXOffset;}
+    void DecScreenXOffset(void) {if (screenTileXOffset>0) --screenTileXOffset;}
+    int GetScreenXOffset(void) {return screenTileXOffset;}
+    void IncSelOffset(void) {++selTileYOffset;}
+    void DecSelOffset(void) {if (selTileYOffset>0) --selTileYOffset;}
+    int GetSelOffset(void) {return selTileYOffset;}
     void PosScreenToTile(int* x, int* y);
     void PosScreenToTileRound(int* x, int* y);
     void PosTileToScreen(int* x, int* y);
